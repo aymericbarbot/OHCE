f9ca012ba7e21a1560efeab6275e4c22f3c36c55
aymericbarbot <aymeric.barbot@neusta.fr>
2025-06-11 10:46:28 +0200

    test miroir rouge

--- test_ohce_acceptance.py
+++ test_ohce_acceptance.py
@@ -4,3 +4,7 @@ from pathlib import Path
 def test_pytest_marche():
     assert True is True
 
+def test_miroir_simple():
+    out = run_ohce("test\n")
+    # On veut que "tset" apparaisse dans la sortie, peu importe la position
+    assert "tset" in out
